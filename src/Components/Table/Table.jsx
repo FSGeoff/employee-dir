66f2737f93266d2151add3c7c6a9da39df785289
Geoff Nix <FullStackGeoff@gmail.com>
2021-01-26 20:36:17 -0500

    resumed last repo and making changes in this new repo

---
+++ src/Components/Table/Table.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Table = () => {
+	return (
+		<table className="table table-dark table-hover table-striped">
+			<thead>
+				<tr>
+					<th scope="col">0390511T</th>
+					<th scope="col">
+						<img src="https://randomuser.me/api/portraits/thumb/men/75.jpg" />
+					</th>
+					<th scope="col">Tom Collins</th>
+					<th scope="col">29</th>
+					<th scope="col">555-1212</th>
+					<th scope="col">email@mail.com</th>
+				</tr>
+			</thead>
+			<tbody>
+			
+				
+				
+			</tbody>
+		</table>
+	);
+};
+
+export default Table;
