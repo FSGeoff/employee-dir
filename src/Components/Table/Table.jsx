f1ebc36af0f736496733e0c6918632285a10092f
Geoff Nix <FullStackGeoff@gmail.com>
2021-01-26 21:58:08 -0500

    added sort by country

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -2,25 +2,48 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Table = () => {
-	const [usersState, setUsersState] = useState([]);
+    const [usersState, setUsersState] = useState([]);
+    const [usersToDisplay, setUsersToDisplay] = useState([]);
+
+
 
 	useEffect(() => {
 		axios
 			.get("https://randomuser.me/api/?results=250")
 			.then((response) => {
-                console.log(response.data.results);
+				console.log(response.data.results);
                 setUsersState(response.data.results);
+                setUsersToDisplay(response.data.results);
 			})
 			.catch((err) => {
 				console.log(err);
 			});
 	}, []);
 
+	const sortByCountry = () => {
+        let tempUsers = [...usersState]
+		const sortedUsers = tempUsers.sort((a, b) => {
+			const aVal = a.location.country;
+			const bVal = b.location.country;
+			if (aVal < bVal) {
+				return -1;
+			}
+			if (aVal > bVal) {
+				return 1;
+			}
+			return 0;
+        });
+        console.log(tempUsers);
+        setUsersState(tempUsers);
+	};
+
 	return (
 		<table className="table table-dark table-hover ">
 			<thead>
 				<tr>
-					<th scope="col">ID</th>
+					<th scope="col" onClick={sortByCountry}>
+						Country
+					</th>
 					<th scope="col">Image</th>
 					<th scope="col">Name</th>
 					<th scope="col">Age</th>
@@ -29,11 +52,14 @@ const Table = () => {
 				</tr>
 			</thead>
 			<tbody>
-				{usersState.map((user) => (
-					<tr>
-						<td>{user.id.value}</td>
+				{usersState.map((user, index) => (
+					<tr key={index}>
+						<td>{user.location.country}</td>
 						<td>
-							<img alt="thumbnail" src={user.picture.thumbnail} />
+							<img
+								alt={user.name.first}
+								src={user.picture.thumbnail}
+							/>
 						</td>
 						<td>
 							{user.name.first} {user.name.last}
